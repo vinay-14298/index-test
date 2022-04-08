38f526486363d28701da920553e57b47683553f5
Minnakanti Vinay Babu <73283398+vinay-14298@users.noreply.github.com>
2022-04-08 15:23:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
+    <title>PHP - Hello, World Vinay Babu Minnakanti!</title>
 </head>
 <body>
         <h1><?php echo 'Hello, World!'; ?></h1>
